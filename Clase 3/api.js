e6512653c058126d874172a412d7d6394cc41fff
DV-Axel <axel.cichello@davinci.edu.ar>
2024-08-27 21:57:17 -0300

    clase 3

--- Clase 3/api.js
+++ Clase 3/api.js
@@ -1,1 +1,10 @@
-const express = requere("express")
+const express = require("express")
+const api=express();
+const router = express.Router()
+
+const host = "localhost"
+const port = 3999
+
+api.listen(port, host, () => {
+    console.log("ok cambio")
+})
