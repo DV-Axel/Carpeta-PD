7556e494b8fd538a52f14abafca82d5fc49bed29
DV-Axel <axel.cichello@davinci.edu.ar>
2024-08-27 22:19:28 -0300

    clase 3

--- Clase 3/api.js
+++ Clase 3/api.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const api=express();
 const router = express.Router()
-
+const path=require("path") //uso path para manejar mejor las rutas
 const host = "localhost"
 const port = 3999
 
+api.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,("./public/asd.html"))) //la carpeta raiz seria api.js; sendFile para mostrar una pagina
+})
+
+
 api.listen(port, host, () => {
     console.log("ok cambio")
+    console.log(`${host}:${port}`)
 })
