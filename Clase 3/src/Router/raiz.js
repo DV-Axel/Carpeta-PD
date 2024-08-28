3d4ed1047d9cfab89ce1ed93aabe0e5a97b43621
DV-Axel <axel.cichello@davinci.edu.ar>
2024-08-28 11:12:52 -0300

    Update raiz.js
    cambie como esta en el rar del profe, falta probar

--- Clase 3/src/Router/raiz.js
+++ Clase 3/src/Router/raiz.js
@@ -2,12 +2,13 @@ const express = require("express") //traigo el modulo de express
 const router = express.Router()
 const path = require("path")
 
- router.get("/", (req, res) => {
-     res.sendFile(path.join(__dirname,("../../public"))) //la carpeta raiz seria api.js; sendFile para mostrar una pagina
+router.use("/", express.static(path.join(__dirname, "../../public")))
 
+ router.get("/*", (req, res) => {
+     res.sendFile(path.join(path.join(__dirname,"../../public"))) //la carpeta raiz seria api.js; sendFile para mostrar una pagina
+     
      //res.send("<h1>estoy</h1>") prueba solo etiqueta anda
  })
-
 
 module.exports={
     router
